8a08013577c15a427959401108e46335050f2bcd
Max Dizy <47869939+maxdizy@users.noreply.github.com>
2021-02-05 16:56:32 -0500

    Add files via upload
    server test

---
+++ battleBashClient.java
@@ -0,0 +1,8 @@
+import java.net.*;
+import java.io.*;
+
+public class battleBashClient{
+  public static void main(String[] args) throws IOException{
+    Socket s = new Socket("localhost", 4999);
+  }
+}
