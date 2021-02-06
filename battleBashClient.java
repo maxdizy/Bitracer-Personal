1b2444420b689513e6a5204e56aabdca36e6bd02
Max Dizy <47869939+maxdizy@users.noreply.github.com>
2021-02-05 19:44:18 -0500

    Add files via upload
    working locally

--- battleBashClient.java
+++ battleBashClient.java
@@ -1,8 +1,33 @@
-import java.net.*;
-import java.io.*;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class battleBashClient{
-  public static void main(String[] args) throws IOException{
-    Socket s = new Socket("localhost", 4999);
+  public static void main(String[] args) throws Exception{
+    try{
+      String host = "localhost";
+      int port = 10430;
+
+      Socket client = new Socket("192.168.56.1", 4444);
+
+      ObjectOutputStream clientOut = new ObjectOutputStream(client.getOutputStream());
+      //ObjectOutputStream serverOut = new ObjectOutputStream(connection.getOutputStream());
+      //ObjectInputStream clientIn = new ObjectInputStream(client.getInputStream());
+      //ObjectInputStream serverIn = new ObjectInputStream(connection.getInputStream());
+      System.out.println("Communication ready...");
+
+      String messageOut = "Hello World";
+      clientOut.writeObject(messageOut);
+      clientOut.flush();
+      System.out.println("\"" + messageOut + "\"" + " was sent to the server");
+      clientOut.close();
+      client.close();
+    }
+    catch(Exception e){
+      System.out.println("Client Failed...");
+      System.out.println(e);
+    }
   }
 }
