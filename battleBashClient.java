718b2a3ab38f66229b7bd814cccd2cc518995d4c
Max Dizy <47869939+maxdizy@users.noreply.github.com>
2021-02-05 20:35:54 -0500

    Add files via upload
    working

--- battleBashClient.java
+++ battleBashClient.java
@@ -7,14 +7,11 @@ import java.net.Socket;
 public class battleBashClient{
   public static void main(String[] args) throws Exception{
     try{
-      String host = "localhost";
-      int port = 10430;
-
-      Socket client = new Socket("192.168.56.1", 4444);
+      Socket client = new Socket("192.168.0.110", 4444);
 
       ObjectOutputStream clientOut = new ObjectOutputStream(client.getOutputStream());
       //ObjectOutputStream serverOut = new ObjectOutputStream(connection.getOutputStream());
-      //ObjectInputStream clientIn = new ObjectInputStream(client.getInputStream());
+      ObjectInputStream clientIn = new ObjectInputStream(client.getInputStream());
       //ObjectInputStream serverIn = new ObjectInputStream(connection.getInputStream());
       System.out.println("Communication ready...");
 
@@ -22,6 +19,11 @@ public class battleBashClient{
       clientOut.writeObject(messageOut);
       clientOut.flush();
       System.out.println("\"" + messageOut + "\"" + " was sent to the server");
+
+      String messageIn = (String)clientIn.readObject();
+      System.out.println("Message recieved from server: " + "\"" + messageIn + "\"");
+
+
       clientOut.close();
       client.close();
     }
