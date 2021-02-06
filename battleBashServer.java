bc773c5d5df92c538ca8d89fb65932f475a25a74
Max Dizy <47869939+maxdizy@users.noreply.github.com>
2021-02-05 20:36:18 -0500

    Add files via upload

--- battleBashServer.java
+++ battleBashServer.java
@@ -6,35 +6,38 @@ import java.net.Socket;
 
 public class battleBashServer{
   public static void main(String[] args) throws Exception{
-    String host = "localhost";
-    int port = 10430;
+    try{
+      ServerSocket server = new ServerSocket(4444);
 
-    ServerSocket server = new ServerSocket(port, 50, InetAddress.getByName(host));
+      System.out.println("Server Started...");
+      Socket connection = server.accept();
+      System.out.println("Successfully Connected");
 
-    System.out.println("Server Started...");
-    Socket client = new Socket(host, port);
-    System.out.println("Connecting to Server...");
-    Socket connection = server.accept();
-    System.out.println("Successfully Connected");
+      //ObjectOutputStream clientOut = new ObjectOutputStream(client.getOutputStream());
+      ObjectOutputStream serverOut = new ObjectOutputStream(connection.getOutputStream());
+      //ObjectInputStream clientIn = new ObjectInputStream(client.getInputStream());
+      ObjectInputStream serverIn = new ObjectInputStream(connection.getInputStream());
+      System.out.println("Communication ready...");
 
-    ObjectOutputStream clientOut = new ObjectOutputStream(client.getOutputStream());
-    ObjectOutputStream serverOut = new ObjectOutputStream(connection.getOutputStream());
-    ObjectInputStream clientIn = new ObjectInputStream(client.getInputStream());
-    ObjectInputStream serverIn = new ObjectInputStream(connection.getInputStream());
-    System.out.println("Communication ready...");
+      String messageIn = (String)serverIn.readObject();
+      System.out.println("Message recieved from client: " + "\"" + messageIn + "\"");
 
-    String messageOut = "Hello World";
-    clientOut.writeObject(messageOut);
-    clientOut.flush();
-    System.out.println("\"" + messageOut + "\"" + " was sent to the server");
+      String messageOut = "What's Up Max";
+      serverOut.writeObject(messageOut);
+      serverOut.flush();
+      System.out.println("\"" + messageOut + "\"" + " was sent to the server");
+      //clientOut.close();
+      //serverOut.close();
 
-    String messageIn = (String)serverIn.readObject();
-    System.out.println("Message recieved from client: " + messageIn);
-
-    clientOut.close();
-    serverOut.close();
-    System.out.println("Connections Closed");
-    server.close();
-    System.out.println("Server Terminated");
+      serverIn.close();
+      serverOut.close();
+      System.out.println("Connections Closed");
+      server.close();
+      System.out.println("Server Terminated");
+    }
+    catch(Exception e){
+      System.out.println("Server Failed...");
+      System.out.println(e);
+    }
   }
 }
